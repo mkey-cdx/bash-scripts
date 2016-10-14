05610e6420c9d62537903c0899dbda471a88846a
mkey-cdx <mickael.coiraton@gmail.com>
2016-10-14 18:05:06 +0200

    Add files via upload

---
+++ ssh_portknock.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+# ============================================================================== 
+# SSH knockd connection script.
+#
+# Author: Mickaël Coiraton
+# Version 0.1.5
+# Updated: 14nd October 2016
+#
+# This script sends port-hits sequence to knockd deamon and checks 
+# remote ssh port state before connecting.
+# Please edit theses variable at your convenience.
+#
+
+# IP address, hostname or fqdn.
+hostname="host.example.com"     
+# Remote port number to scan.     
+port=22
+# Sequence sent by kockd client.
+sequence="4900:tcp 6566:udp 4030:udp"
+
+# ==============================================================================
+
+
+# Installed package checking function.
+function check_package(){
+    if ( ! which $1 > /dev/null 2>&1 ); then
+        echo "Warning: $1 package has not been found in this system."
+        echo "Please install it before using this tool."
+        exit 1
+    fi
+}
+
+
+# Check variables.
+if [ -z "$hostname" ] || [ -z "$sequence" ] || [ -z "$port" ]; then
+    echo "You must provide the remote server information."
+    echo "Please edit the variables before using this script."
+    exit
+fi
+
+
+# Check installed packages.
+check_package knock
+check_package nc
+
+
+# Check remote host availability.
+echo -n "Checking host availability..."
+ping -c 1 $hostname > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+    echo -e "\nRemote host is unreachable: $hostname"
+    exit 1
+else
+    echo "done."
+fi
+
+
+# Send knocking sequence.
+echo -n "Sending knocking sequence..."
+knock -d 1000 $hostname $sequence
+echo "done."
+
+echo -n "Scanning $port port state..."
+nc -z -w 2 $hostname $port
+
+
+# Initiate ssh session.
+if [ $? -eq 0 ]; then
+    echo "server port is open."
+    echo -e "Starting ssh session...\n"
+    ssh -p $port $hostname
+    exit 0
+else
+    echo -e "\n[Error]" $port "server port is closed."
+    exit 1
+fi
+
